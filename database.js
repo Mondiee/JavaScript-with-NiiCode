d9a378708e7c5a9dbcff1758f4d3f0e6ef9cb19a
niicode <niidarku66@gmail.com>
2022-06-10 22:55:23 +0000

    make changes to your code using index as eg.

---
+++ database.js
@@ -0,0 +1,27 @@
+const foods = {
+    dishOne: {
+        name: "Rice",
+        id:"1",
+        "type": "vegetable",
+    },
+    dishTwo: {
+        name: "Beef",
+        id:"2",
+        "type": "meat",
+    },
+    dishThree: {
+        name: "Chicken",
+        id:"3",
+        "type": "meat",
+    },
+    dishFour: {
+        name: "Pork",
+        id:"4",
+        "type": "meat",
+    },
+    dishFive: {
+        name: "Egg",
+        id:"5",
+        "type": "vegetable",
+    }
+}
